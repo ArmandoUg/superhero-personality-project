446ef428828b5d2665746371c7868fef4efdaa10
Armando Ugalde <uarmando15@yahoo.com>
2022-04-30 14:02:43 -0700

    added files for endpage html, css and java script

---
+++ assets/project.js
@@ -0,0 +1,14 @@
+var marapi = "https://gateway.marvel.com:443/v1/public/characters/1009610?apikey=2a32ba5a2e6e8f0830ec870eb1659e04";
+var descriptionEl = document.querySelector(".description")
+fetch (marapi) 
+.then(function(response){
+    return response.json()
+})
+.then(function(apidata){
+    console.log(apidata);
+    var descText =apidata.data.results[0].description;
+    descriptionEl.textContent = descText;
+})
+// var marimg ="http://i.annihil.us/u/prod/marvel/i/mg/8/a0/523ca6f2b11e4/portrait_xlarge.jpg"
+// var image = document.querySelector(".test");
+// image.append(marimg);
