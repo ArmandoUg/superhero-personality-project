1817c6c835867399a697662c81ca85117dcd382a
Armando Ugalde <uarmando15@yahoo.com>
2022-05-03 20:10:14 -0700

    added js code to append an thumnnail that is pulled from api data. added a 404 page to catch bad calls

--- assets/project.js
+++ assets/project.js
@@ -1,6 +1,15 @@
-var marapi = "https://gateway.marvel.com:443/v1/public/characters/1009610?apikey=2a32ba5a2e6e8f0830ec870eb1659e04";
+//when page3 first loads, get url param from url
+var characterCode = location.search.split("=")[1];
+console.log('character code --- ', characterCode)
+
+
+
+
+
+var marapi = `https://gateway.marvel.com:443/v1/public/characters/${characterCode}?apikey=2a32ba5a2e6e8f0830ec870eb1659e04`;
 var descriptionEl = document.querySelector(".description");
-var nameEl = document.querySelector(".name");
+var nameEl = document.querySelector(".cname");
+var imgEl= document.querySelector(".chara-img-block");
 fetch (marapi) 
 .then(function(response){
     return response.json()
@@ -11,8 +20,19 @@ fetch (marapi)
     descriptionEl.textContent = descText;
     var nameText =apidata.data.results[0].name;
     nameEl.textContent= nameText;
+    var imgapidata= apidata.data.results[0].thumbnail.path;
+   console.log(imgapidata+"/portrait_incredible.jpg");
+   var image= document.createElement("img");
+   image.setAttribute("src",imgapidata+"/portrait_incredible.jpg");
+   imgEl.append(image);
 
+}).catch(function(err){
+    // console.log(err);
+    // setTimeout(function(){
+    //     console.log("couldn't find a character... redirecting in 5 seconds...")
+    //     location.replace('./fourofour.html')
+    // }, 5000)
 })
 // var marimg ="http://i.annihil.us/u/prod/marvel/i/mg/8/a0/523ca6f2b11e4/portrait_xlarge.jpg"
 // var image = document.querySelector(".test");
-// image.append(marimg);
+// image.append(marimg); portrait_incredible.jpg
